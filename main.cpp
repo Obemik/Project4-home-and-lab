2fbda526cb07bfd8ad2fd4f86ac574c6ab2d8ab6
Obemik <misaperepecaj@gmail.com>
2024-04-17 20:19:21 +0300

    lab ez1 done

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h> // Для використання SetConsoleTextAttribute
+using namespace std;
+
+int main() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, 7);
+    cout << "                Name       : ";
+    SetConsoleTextAttribute(hConsole, 6);
+    cout << "\"The war and the peace\"\n";
+    SetConsoleTextAttribute(hConsole, 7);
+    cout << "                Avtor      :   L.N. Tolstoj\n";
+    cout << "                Izdatelstvo:  Piter\n";
+    cout << "                Pages      : ";
+    SetConsoleTextAttribute(hConsole, 6);
+    cout << "500\n";
+    SetConsoleTextAttribute(hConsole, 7);
+    cout << endl;
+    return 0;
+}
